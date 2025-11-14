24c0ec95ebb29b9c15f0fbd350ce6b8a440a9968
Nabin Mulepati <nmulepati@nvidia.com>
2025-11-14 13:15:32 -0700

    update logging for ftue new default config files creation

--- src/data_designer/config/default_model_settings.py
+++ src/data_designer/config/default_model_settings.py
@@ -2,19 +2,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import logging
-import os
-from typing import Literal, Optional
+from typing import Literal
 
 from ..cli.utils import load_config_file, save_config_file
 from .models import InferenceParameters, ModelConfig, ModelProvider
 from .utils.constants import (
     MODEL_CONFIGS_FILE_PATH,
     MODEL_PROVIDERS_FILE_PATH,
-    NVIDIA_API_KEY_ENV_VAR_NAME,
-    OPENAI_API_KEY_ENV_VAR_NAME,
     PREDEFINED_PROVIDERS,
     PREDEFINED_PROVIDERS_MODEL_MAP,
 )
+from .utils.info import ConfigBuilderInfo, InfoType, InterfaceInfo
 
 logger = logging.getLogger(__name__)
 
@@ -72,7 +70,7 @@ def get_default_model_configs() -> list[ModelConfig]:
     if MODEL_CONFIGS_FILE_PATH.exists():
         config_dict = load_config_file(MODEL_CONFIGS_FILE_PATH)
         if "model_configs" in config_dict:
-            logger.info(f"♻️ Found default model configs in {str(MODEL_CONFIGS_FILE_PATH)!r}")
+            logger.info(f"♻️ Using default model configs from {str(MODEL_CONFIGS_FILE_PATH)!r}")
             return [ModelConfig.model_validate(mc) for mc in config_dict["model_configs"]]
     raise FileNotFoundError(f"Default model configs file not found at {str(MODEL_CONFIGS_FILE_PATH)!r}")
 
@@ -81,28 +79,28 @@ def get_default_providers() -> list[ModelProvider]:
     if MODEL_PROVIDERS_FILE_PATH.exists():
         config_dict = load_config_file(MODEL_PROVIDERS_FILE_PATH)
         if "providers" in config_dict:
-            logger.info(f"♻️ Found default model providers in {str(MODEL_PROVIDERS_FILE_PATH)!r}")
+            logger.info(f"♻️ Using default model providers from {str(MODEL_PROVIDERS_FILE_PATH)!r}")
             return [ModelProvider.model_validate(p) for p in config_dict["providers"]]
     raise FileNotFoundError(f"Default model providers file not found at {str(MODEL_PROVIDERS_FILE_PATH)!r}")
 
 
-def get_nvidia_api_key() -> Optional[str]:
-    return os.getenv(NVIDIA_API_KEY_ENV_VAR_NAME)
-
-
-def get_openai_api_key() -> Optional[str]:
-    return os.getenv(OPENAI_API_KEY_ENV_VAR_NAME)
-
-
 def resolve_seed_default_model_settings() -> None:
     if not MODEL_CONFIGS_FILE_PATH.exists():
-        logger.debug(f"Creating default model configs file at {str(MODEL_CONFIGS_FILE_PATH)!r}")
+        logger.info(
+            f"🍾 Default model configs were not found, so writing the following to {str(MODEL_CONFIGS_FILE_PATH)!r}"
+        )
+        config_builder_info = ConfigBuilderInfo(model_configs=get_builtin_model_configs())
+        config_builder_info.display(info_type=InfoType.MODEL_CONFIGS)
         save_config_file(
             MODEL_CONFIGS_FILE_PATH, {"model_configs": [mc.model_dump() for mc in get_builtin_model_configs()]}
         )
 
     if not MODEL_PROVIDERS_FILE_PATH.exists():
-        logger.debug(f"Creating default model providers file at {str(MODEL_PROVIDERS_FILE_PATH)!r}")
+        logger.info(
+            f"🪄  Default model providers were not found, so writing the following to {str(MODEL_PROVIDERS_FILE_PATH)!r}"
+        )
+        interface_info = InterfaceInfo(model_providers=get_builtin_model_providers())
+        interface_info.display(info_type=InfoType.MODEL_PROVIDERS)
         save_config_file(
             MODEL_PROVIDERS_FILE_PATH, {"providers": [p.model_dump() for p in get_builtin_model_providers()]}
         )
