703487e68faee2293d76066255ee20fc2a9d08cd
Mike Knepper <mknepper@nvidia.com>
2025-12-03 21:35:57 -0600

    Refactor config and essentials exports

--- src/data_designer/essentials/__init__.py
+++ src/data_designer/essentials/__init__.py
@@ -1,143 +1,25 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
+
+from data_designer.config.exports import *
+from data_designer.config.validator_params import LocalCallableValidatorParams
+from data_designer.interface.data_designer import DataDesigner
 from data_designer.logging import LoggingConfig, configure_logging
 
 configure_logging(LoggingConfig.default())
 
-from data_designer.config.analysis.column_profilers import JudgeScoreProfilerConfig
-from data_designer.config.column_configs import (
-    ExpressionColumnConfig,
-    LLMCodeColumnConfig,
-    LLMJudgeColumnConfig,
-    LLMStructuredColumnConfig,
-    LLMTextColumnConfig,
-    SamplerColumnConfig,
-    Score,
-    SeedDatasetColumnConfig,
-    ValidationColumnConfig,
-)
-from data_designer.config.column_types import DataDesignerColumnType
-from data_designer.config.config_builder import DataDesignerConfigBuilder
-from data_designer.config.data_designer_config import DataDesignerConfig
-from data_designer.config.dataset_builders import BuildStage
-from data_designer.config.datastore import DatastoreSettings
-from data_designer.config.models import (
-    ImageContext,
-    ImageFormat,
-    InferenceParameters,
-    ManualDistribution,
-    ManualDistributionParams,
-    Modality,
-    ModalityContext,
-    ModalityDataType,
-    ModelConfig,
-    UniformDistribution,
-    UniformDistributionParams,
-)
-from data_designer.config.processors import DropColumnsProcessorConfig, ProcessorType
-from data_designer.config.sampler_constraints import ColumnInequalityConstraint, ScalarInequalityConstraint
-from data_designer.config.sampler_params import (
-    BernoulliMixtureSamplerParams,
-    BernoulliSamplerParams,
-    BinomialSamplerParams,
-    CategorySamplerParams,
-    DatetimeSamplerParams,
-    GaussianSamplerParams,
-    PersonFromFakerSamplerParams,
-    PersonSamplerParams,
-    PoissonSamplerParams,
-    SamplerType,
-    ScipySamplerParams,
-    SubcategorySamplerParams,
-    TimeDeltaSamplerParams,
-    UniformSamplerParams,
-    UUIDSamplerParams,
-)
-from data_designer.config.seed import (
-    DatastoreSeedDatasetReference,
-    IndexRange,
-    PartitionBlock,
-    SamplingStrategy,
-    SeedConfig,
-)
-from data_designer.config.utils.code_lang import CodeLang
-from data_designer.config.utils.info import InfoType
-from data_designer.config.utils.misc import can_run_data_designer_locally
-from data_designer.config.validator_params import (
-    CodeValidatorParams,
-    RemoteValidatorParams,
-    ValidatorType,
-)
 
-local_library_imports = []
-try:
-    if can_run_data_designer_locally():
-        from data_designer.config.validator_params import LocalCallableValidatorParams  # noqa: F401
-        from data_designer.engine.model_provider import ModelProvider  # noqa: F401
-        from data_designer.interface.data_designer import DataDesigner  # noqa: F401
+def get_essentials_exports() -> list[str]:
+    logging = [
+        configure_logging.__name__,
+        LoggingConfig.__name__,
+    ]
+    local = [
+        DataDesigner.__name__,
+        LocalCallableValidatorParams.__name__,
+    ]
 
-        local_library_imports = ["DataDesigner", "LocalCallableValidatorParams", "ModelProvider"]
-except ModuleNotFoundError:
-    pass
+    return logging + local + get_config_exports()
 
-__all__ = [
-    "BernoulliMixtureSamplerParams",
-    "BernoulliSamplerParams",
-    "BinomialSamplerParams",
-    "CategorySamplerParams",
-    "CodeLang",
-    "CodeValidatorParams",
-    "ColumnInequalityConstraint",
-    "configure_logging",
-    "DataDesignerColumnType",
-    "DataDesignerConfig",
-    "DataDesignerConfigBuilder",
-    "BuildStage",
-    "DatastoreSeedDatasetReference",
-    "DatastoreSettings",
-    "DatetimeSamplerParams",
-    "DropColumnsProcessorConfig",
-    "ExpressionColumnConfig",
-    "GaussianSamplerParams",
-    "IndexRange",
-    "InfoType",
-    "ImageContext",
-    "ImageFormat",
-    "InferenceParameters",
-    "JudgeScoreProfilerConfig",
-    "LLMCodeColumnConfig",
-    "LLMJudgeColumnConfig",
-    "LLMStructuredColumnConfig",
-    "LLMTextColumnConfig",
-    "LoggingConfig",
-    "ManualDistribution",
-    "ManualDistributionParams",
-    "Modality",
-    "ModalityContext",
-    "ModalityDataType",
-    "ModelConfig",
-    "PartitionBlock",
-    "PersonSamplerParams",
-    "PersonFromFakerSamplerParams",
-    "PoissonSamplerParams",
-    "ProcessorType",
-    "RemoteValidatorParams",
-    "SamplerColumnConfig",
-    "SamplerType",
-    "SamplingStrategy",
-    "ScalarInequalityConstraint",
-    "ScipySamplerParams",
-    "Score",
-    "SeedConfig",
-    "SeedDatasetColumnConfig",
-    "SubcategorySamplerParams",
-    "TimeDeltaSamplerParams",
-    "UniformDistribution",
-    "UniformDistributionParams",
-    "UniformSamplerParams",
-    "UUIDSamplerParams",
-    "ValidationColumnConfig",
-    "ValidatorType",
-]
 
-__all__.extend(local_library_imports)
+__all__ = get_essentials_exports()
