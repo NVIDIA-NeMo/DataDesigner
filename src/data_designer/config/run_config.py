b68d89ff6724e17cd6cb36c9e04a8b4672162a78
Eric W. Tramel <eric.tramel@gmail.com>
2026-01-08 13:59:31 -0500

    Backwards Compat for python 3.10

--- src/data_designer/config/run_config.py
+++ src/data_designer/config/run_config.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Self
+from typing_extensions import Self
 
 from pydantic import Field, model_validator
 
