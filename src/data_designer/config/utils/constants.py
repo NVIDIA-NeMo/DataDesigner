6c9ad822fab2d408f694dafd79f8e6189c5e860f
Johnny Greco <jogreco@nvidia.com>
2026-01-26 15:42:02 -0500

    add locales

--- src/data_designer/config/utils/constants.py
+++ src/data_designer/config/utils/constants.py
@@ -353,9 +353,11 @@ PREDEFINED_PROVIDERS_MODEL_MAP = {
 NEMOTRON_PERSONAS_DATASET_SIZES = {
     "en_US": "1.24 GB",
     "en_IN": "2.39 GB",
+    "en_SG": "4.9 GB",
     "hi_Deva_IN": "4.14 GB",
     "hi_Latn_IN": "2.7 GB",
     "ja_JP": "1.69 GB",
+    "pt_BR": "4.1 GB",
 }
 
 LOCALES_WITH_MANAGED_DATASETS = list[str](NEMOTRON_PERSONAS_DATASET_SIZES.keys())
