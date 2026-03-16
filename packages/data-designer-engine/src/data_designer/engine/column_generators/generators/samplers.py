6f19cac42da7bff06d89de33c1edc14d8cb71c5e
Mike Knepper <mknepper@nvidia.com>
2026-03-16 16:19:25 -0500

    Check for None

--- packages/data-designer-engine/src/data_designer/engine/column_generators/generators/samplers.py
+++ packages/data-designer-engine/src/data_designer/engine/column_generators/generators/samplers.py
@@ -11,6 +11,7 @@ from typing import TYPE_CHECKING, Callable
 from data_designer.config.utils.constants import LOCALES_WITH_MANAGED_DATASETS
 from data_designer.engine.column_generators.generators.base import FromScratchColumnGenerator, GenerationStrategy
 from data_designer.engine.dataset_builders.multi_column_configs import SamplerMultiColumnConfig
+from data_designer.engine.errors import DataDesignerRuntimeError
 from data_designer.engine.processing.utils import concat_datasets
 from data_designer.engine.resources.managed_dataset_generator import ManagedDatasetGenerator
 from data_designer.engine.sampling_gen.data_sources.sources import SamplerType
@@ -43,7 +44,12 @@ class SamplerColumnGenerator(FromScratchColumnGenerator[SamplerMultiColumnConfig
 
     @property
     def _person_generator_loader(self) -> Callable[[bool], ManagedDatasetGenerator]:
-        return partial(load_person_data_sampler, reader=self.resource_provider.person_reader)
+        if (person_reader := self.resource_provider.person_reader) is None:
+            raise DataDesignerRuntimeError(
+                "Cannot perform person generation because no PersonReader has been configured. "
+                "Ensure a PersonReader is set on the ResourceProvider."
+            )
+        return partial(load_person_data_sampler, reader=person_reader)
 
     def _create_sampling_dataset_generator(self) -> SamplingDatasetGenerator:
         return SamplingDatasetGenerator(
